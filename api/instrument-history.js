e979f8b321d361b98298a328260018775e3b16f1
David Jurelius <davan839@gmail.com>
2020-11-25 23:04:30 +0100

    async fix

--- api/instrument-history.js
+++ api/instrument-history.js
@@ -2,14 +2,14 @@ const puppeteer = require('puppeteer');
 const html_table_to_json = require('html-table-to-json');
 
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
 
   [ url, selector ] = req.query;
 
   url = decodeURIComponent(url);
   selector = decodeURIComponent(selector);
 
-  //(async () => {
+  const result = (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
   
@@ -18,10 +18,11 @@ module.exports = async (req, res) => {
     const html_table = await page.$eval(xact_selector, e => e.outerHTML);
     const xact_obligation = html_table_to_json.parse(html_table).results;
     await browser.close();
-//  })();
+    return xact_obligation;
+  })();
 
   res.json({
-    xact_obligation,
+    result,
     query: req.query,
   })
 }
