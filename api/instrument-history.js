afa43e151ed2ba754edc7fd1266ffa4ada8fb023
David Jurelius <davan839@gmail.com>
2020-11-26 06:38:46 +0100

    some stuff

--- api/instrument-history.js
+++ api/instrument-history.js
@@ -1,28 +1,28 @@
 const puppeteer = require('puppeteer');
 const html_table_to_json = require('html-table-to-json');
 
-
-module.exports = (req, res) => {
+const instrument_history = async (req, res) => {
 
   [ url, selector ] = req.query;
 
   url = decodeURIComponent(url);
   selector = decodeURIComponent(selector);
+console.log(url, selector);
+  // const browser = await puppeteer.launch();
+  // const page = await browser.newPage();
 
-  const result = (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-  
-    await page.goto(url);
-    await page.waitForSelector(selector || "instrumenthistory history table tbody > tr:nth-child(1) > td.cell-table__num");
-    const html_table = await page.$eval(xact_selector, e => e.outerHTML);
-    const xact_obligation = html_table_to_json.parse(html_table).results;
-    await browser.close();
-    return xact_obligation;
-  })();
-
+  // await page.goto(url);
+  // await page.waitForSelector(selector || "instrumenthistory history table tbody > tr:nth-child(1) > td.cell-table__num");
+  // const html_table = await page.$eval(xact_selector, e => e.outerHTML);
+  // const xact_obligation = html_table_to_json.parse(html_table).results;
+  // await browser.close();
+  console.log(xact_obligation);
   res.json({
-    result,
+    //xact_obligation,
     query: req.query,
+    url,
+    selector
   })
 }
+
+module.exports = instrument_history
